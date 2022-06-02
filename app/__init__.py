6409b5114a9521d73e6604ace4daa4919bf8acf4
Roberto Torres <roberto@umba.com>
2022-06-02 02:02:33 -0500

    Refactor Flask Application Init

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
+import os
 from flask import Flask
 
 from .config import config
 from .controllers.url_shortener import url_shortener_api
 
 
-def create_app(config_name):
+def create_app(config_name=os.getenv("FLASK_ENV") or "development"):
+    # create and configure the app
     app = Flask(__name__)
     # Configuration
     app.config.from_object(config[config_name])
